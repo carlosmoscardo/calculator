9302e401fd7a6729a550259fa14f0cc5ed8398f6
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-10-15 10:33:29 +0200

    Add clearer process comments

--- script.js
+++ script.js
@@ -32,6 +32,10 @@ function operate(num1, operator, num2) {
 }
 
 // Haz funciones que muestren los números en pantalla cuando pulses los botones
-// 1. 
-
-// Además, guarda ese número en una variable para poder usarlo después.
+// 1. Guardar en variables el textContent del nodo de los siguientes botones: dígitos [0, 9], decimal (.) y operadores (+, -, *, /) => pensar como hacerlo DRY
+// 1.1 Hacer display de cada variable en la pantalla de la calculadora cuando el botón es pulsado
+// 2. Guardar en variables el nodo de los botones AC, ⌫, y =
+// 2.1 Añadir la correspondiente funcionalidad a cada botón
+// 2.2 AC debe limpiar tanto visualmente como internamente la info recibida por la calculadora.
+// 2.3 ⌫ debe eliminar tanto visualmente como internamente el último elemento escrito en la calculadora
+// 2.4 = debe hacer display del resultado de la operación
